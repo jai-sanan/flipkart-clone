a0d00ffdf77f40e1f0d14d1823d6fcab5175a5c3
anuragsati <anuragsati7@gmail.com>
2023-09-24 16:20:34 +0530

    Fix database connection issue

--- backend/server.js
+++ backend/server.js
@@ -18,15 +18,10 @@ app.use('/' , Router);
 
 const PORT = process.env.PORT || 8000;
 
-const USERNAME = process.env.DB_USERNAME;
-const PASSWORD = process.env.DB_PASSWORD;
+const USERNAME = encodeURIComponent(process.env.DB_USERNAME);
+const PASSWORD = encodeURIComponent(process.env.DB_PASSWORD);
 
-const URL =  process.env.MONGODB_URI || `mongodb://${USERNAME}:${PASSWORD}@ac-nf2usxp-shard-00-00.pez4gpi.mongodb.net:27017,ac-nf2usxp-shard-00-01.pez4gpi.mongodb.net:27017,ac-nf2usxp-shard-00-02.pez4gpi.mongodb.net:27017/Cluster0?ssl=true&replicaSet=atlas-k1adma-shard-0&authSource=admin&retryWrites=true&w=majority`
-
-
-Connection(URL);
-
-app.listen(PORT , () => console.log(`Server is running Successfully on PORT ${PORT} `));
+Connection(USERNAME, PASSWORD);
 
 DefaultData();
 
@@ -43,5 +38,4 @@ paytmParams['CALLBACK_URL']= 'http://localhost:8000/callback';
 paytmParams['EMAIL']= 'jai.sanan@gmail.com';
 paytmParams['MOBILE_NO']='1234567890';
 
-
-
+app.listen(PORT , () => console.log(`Server is running Successfully on PORT ${PORT} `));
